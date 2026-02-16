54b90ea90a67d92a64bb7612473fe353b28a7ad3
Gourab Das <dgourab574@gmail.com>
2026-02-16 17:21:42 +0530

    update

---
+++ src/config/firebaseconfig.ts
@@ -0,0 +1,12 @@
+import admin from "firebase-admin";
+import { readFileSync } from "fs";
+const serviceAccount = JSON.parse(
+  readFileSync("../../firebaseconfig.json", "utf8")
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+export {db};
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,48 @@
-import type { UserSignupData } from "./user.types.js";
+import type { User, UserSignupData } from "./user.types.js";
+import { db } from "../../config/firebaseconfig.js";
+import crypto from "crypto"
 
+const UserCollection = db.collection("UserProfile")
+
+const userIdGenerator = ()=>{
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let id = "";
+    const bytes = crypto.randomBytes(10);
+    for (let i = 0; i < 10; i++) {
+        id += chars[(bytes[i] ?? 0) % chars.length];
+    }
+    return id;
+}
 export const UserSignup = async (userData : UserSignupData)=>{
-    
+
+    const userId = userIdGenerator()
+
+    try {
+        const finalUserData:User = {
+            FullName:userData.FullName,
+            Username:userData.Username,
+            UserID:userId,
+            Description:userData.Description,
+            email:userData.email,
+            phone:userData.phone,
+            Skill_list:userData.Skill_list,
+            Social_profile:userData.Social_profile,
+            Languages:userData.Languages,
+            YOE:userData.YOE,
+            Education:userData.Education,
+            Occupation:userData.Occupation,
+            Location:userData.Location,
+            InterLeet_Coins:0,
+            Confidence_Coins:0,
+            Badges:["First Flight"],
+            Followers:0,
+            Following:0,
+            Premium_Membership:false,
+            Resume:"",
+            User_Analytics:"",
+        }
+        await UserCollection.add(finalUserData)
+    } catch (error:any) {
+        throw new Error("Error Occured!",error);
+    }
 }
